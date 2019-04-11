386dfa175bfcc293a395b5442bf434289d590779
VirtualRcoder <shubhamsh2820@gmail.com>
2019-04-11 18:52:20 +0530

    JS added to react

--- src/index.js
+++ src/index.js
@@ -1,57 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
-
+// import "./index.css";
 //ES6, Nested Components and React Tools
 
-function Books() {
+const btnText = "small button";
+
+function Person() {
+  const btn = "search button";
+  const name = "peter";
+  const lastName = "doe";
   return (
-  	  <section style={{background:"orange"}}>
-      <Book />
-      <Book1 />
+    <section>
+      <button>{btnText}</button>
+      {/* <h2>{name + " " + lastName}</h2> */}
+      <h2>{`${name} ${lastName}`}</h2>
+      <p>info</p>
+      <p>{12 + 35}</p>
+      {/* <p>{let x = 6}</p> */}
     </section>
   );
 }
-const Book = () => {
-  return (
-    <article style={{background:"pink"}}>
-      <CoverImage />
-      <Title />
-      <Author />
-    </article>
-  );
-};
 
-const Book1 = () => {
-  return (
-    <article style={{background:"powderblue"}}>
-      <CoverImage1 />
-      <Title1 />
-      <Author1 />
-    </article>
-  );
-};
-
-const CoverImage = () => (
-  <img style={{ padding: "20px",margin: "20px",background: "#fff",
-    border: "1px solid black"}}
-    width="200"
-    src="https://images-na.ssl-images-amazon.com/images/I/71N4oeWwYlL._AC_UL200_SR200,200_.jpg"
-    alt="wonkey donkey"
-  />
-);
-
-const CoverImage1 = () => (
-  <img style={{ padding: "20px",margin: "20px",background: "#fff",
-    border: "1px solid black"}}
-    width="200"
-    src="https://images-na.ssl-images-amazon.com/images/I/71N4oeWwYlL._AC_UL200_SR200,200_.jpg"
-    alt="wonkey donkey"
-  />
-);
-const Title = () => (<h1 style={{color:"red"}}>The Donkey</h1>);
-const Author = () => <p>by ASR</p>;
-
-const Title1 = () => <h1>The Sad story</h1>;
-const Author1 = () => <p>by shubham sharma</p>;
-
-ReactDom.render(<Books />, document.getElementById("root"));
+ReactDom.render(<Person />, document.getElementById("root"));
