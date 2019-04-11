7bc776c1d42e8c6fdad4e11ca7f657edb87b674e
VirtualRcoder <shubhamsh2820@gmail.com>
2019-04-11 12:56:57 +0530

    retuening it the another way

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//function Greeting(){
+//stateless functional component
+//always return JSX
+
+//	return (<div>
+//		<h1>My first React app</h1>
+//		</div>);
+//}
+/*three arguments element, propsObject, children*/
+
 function Greeting(){
 
-	return <h1>My first React app</h1>;
+	return React.createElement("div",{},"My first react app")
 }
+
 ReactDOM.render(<Greeting/>, document.getElementById('root'));
