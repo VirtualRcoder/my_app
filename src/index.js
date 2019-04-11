c247e554f1318065a5c2957eb55514a8918ecc9b
VirtualRcoder <shubhamsh2820@gmail.com>
2019-04-11 14:12:09 +0530

    Create a page with some images

--- src/index.js
+++ src/index.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-//JSX RULES
-//reutrn single element
-//div / section / article
-//use camelCase for html attribute
-//className instead of class
-//close every element 
-//formatting
+import React from "react";
+import ReactDom from "react-dom";
 
 //ES6, Nested Components and React Tools
 
-function Greeting(){
-
-	return (
-		<section>
-			<Person/>
-			<Message/>
-		</section>		
-		);
+function Books() {
+  return (
+    <section>
+      <Book />
+      <Book1 />
+    </section>
+  );
 }
+const Book = () => {
+  return (
+    <article>
+      <CoverImage />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-const Person =() => <h2>Shubham Sharma</h2>;
-const Message =() => <p>This is my third year</p>;
-ReactDOM.render(<Greeting/>, document.getElementById('root'));
+const Book1 = () => {
+  return (
+    <article>
+      <CoverImage1 />
+      <Title1 />
+      <Author1 />
+    </article>
+  );
+};
+
+const CoverImage = () => (
+  <img
+    width="200"
+    src="https://images-na.ssl-images-amazon.com/images/I/71N4oeWwYlL._AC_UL200_SR200,200_.jpg"
+    alt="wonkey donkey"
+  />
+);
+
+const CoverImage1 = () => (
+  <img
+    width="200"
+    src="https://images-na.ssl-images-amazon.com/images/I/71N4oeWwYlL._AC_UL200_SR200,200_.jpg"
+    alt="wonkey donkey"
+  />
+);
+const Title = () => <h1>The Donkey</h1>;
+const Author = () => <p>by ASR</p>;
+
+const Title1 = () => <h1>The Sad story</h1>;
+const Author1 = () => <p>by shubham sharma</p>;
+
+ReactDom.render(<Books />, document.getElementById("root"));
