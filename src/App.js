e73825082031a0042aef85dce17cda9fe4d11b5c
VirtualRcoder <shubhamsh2820@gmail.com>
2019-04-19 09:08:11 +0530

    Moving from functional based components to class based components using Component from react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-//named and default imports and exports
-//only one default export modules
-//think of module as file
-import React from "react";
+//stateless functional compopnents
+//class based components
+//state, life cycle methods
+//React Hooks
+//Classes - Syntactical Sugar for constructor functions
+//subclasses of components
+//extends from component class
+//inherits functionality
+//Component class is used to transfer from basic E6 class
+import React, { Component } from "react";
 import {name, age, person} from "./data";
 import Banner from "./components/Header/Banner"
 
-const App = () =>  (
- <section>
- 	<Banner/>
-  	<p>This is my content</p>
-  	<p>{name}</p>
-  	<p>{age}</p>
-  	<p>{person.name}</p>
-  </section>
-  );
-//import * as data from "./data";
 //const App = () =>  (
-//  <section>
+// <section>
+// 	<Banner/>
 //  	<p>This is my content</p>
-//  	<p>{data.name}</p>
-//  	<p>{data.age}</p>
-//  	<p>{data.person.name}</p>
+//  	<p>{name}</p>
+//  	<p>{age}</p>
+//  	<p>{person.name}</p>
 //  </section>
 //  );
-
-export default App;
+class App1 extends Component {
+	render(){
+		return (
+			<section>
+			 	<Banner/>
+		  		<p>This is my content</p>
+	 			<p>{name}</p>
+	 			<p>{age}</p>
+	 			<p>{person.name}</p>
+	  		</section>	
+		);
+	}
+}
+export default App1;
